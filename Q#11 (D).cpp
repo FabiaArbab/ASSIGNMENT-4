2d2eeb705cd1860a6cf74f887cedd29b5034ab94
fabia arbab <f2025-0249@bnu.edu.pk>
2025-12-21 02:06:43 +0500

    MQ (Sum of pairs)

---
+++ Q#11 (D).cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, target;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter target sum: ";
+    cin >> target;
+
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] + arr[j] == target) {
+                count++;
+            }
+        }
+    }
+
+    cout << "Number of pairs = " << count;
+    return 0;
+}
